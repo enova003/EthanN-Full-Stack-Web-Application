f15b066e22c1fa03a5dddaf02f6edf0a1834647f
enova003 <enova003@odu.edu>
2024-02-26 18:11:27 -0600

    Milestone1

---
+++ CS418/server/routes/login.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const router = express.Router();
+const database = require("../database");
+const { comparePassword, verifyTOken } = require("../utils/helper");
+const jwt = require("jsonwebtoken");
+const {sendEmail} = require("../utils/sendmail");
+
+function generateRandomFiveDigitNumber() {
+  return Math.floor(10000 + Math.random() * 90000);
+}
+
+/* router.get("/", verifyTOken, (req, res) => {
+  try {
+    database.execute(
+      "select * from user where u_id=?",
+      [req.userId],
+      function (err, result) {
+        if (result == 0) {
+          res.status(401).send("Record not found");
+        } else {
+
+          sendEmail(result[0].u_email,`Login Verification`,`Your one time password is ${oneTimePassword}`);
+
+          res.status(200).send(result);
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+}); */
+
+router.post("/", (req, res) => {
+  try {
+    database.execute(
+      "select * from user where u_email=?",
+      [req.body.u_email],
+      function (err, result) {
+        //console.log("result[0].is_approved: ", result[0].is_approved);
+        if (err || result.length == 0) {
+          res
+            .status(401)
+            .send({
+              status:401,
+              message:"Invalid email and password. Please try again with valid credentials"
+            }
+            );
+        } else {
+          if (comparePassword(req.body.u_password, result[0].u_password)) {
+            //console.log("result[0].is_approved: ", result[0].is_approved);
+            if(result[0].is_approved == 1){
+                const token = jwt.sign(
+                  {
+                    userId: result[0].u_id,
+                    email: req.body.u_email,
+                  },
+                  process.env.TOKEN_SECRET_KEY,
+                  { expiresIn: "1h" }
+                );
+
+                const oneTimePassword = generateRandomFiveDigitNumber();
+
+                res.status(200).send({
+                  data: {
+                    toke: token,
+                    email: req.body.u_email,
+                    userId: result[0].u_id,
+                    verify: oneTimePassword,
+                  },
+                }); 
+
+                sendEmail(result[0].u_email,`Login Verification`,`Your one time password is ${oneTimePassword}`);
+
+              } else {
+              res
+                .status(401)
+                .send({
+                  status:401,
+                  message:"Your email and password are correct, but you have not been approved by the admin yet. Please try again later."
+                }
+                );
+            }
+          } else {
+            res
+              .status(401)
+              .send({
+                status:401,
+                message:"Invalid email and password. Please try again with valid credentials"
+              }
+              );
+          }
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+module.exports = router;
